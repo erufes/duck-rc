5c3a618c448b1411bb47b4abe972a855f114f71f
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-12-16 11:54:41 -0300

    Test support for actual real serial comms

--- ros_ws/src/duck/duck/bridge.py
+++ ros_ws/src/duck/duck/bridge.py
@@ -1,5 +1,6 @@
 import rclpy
 import serial
+import json
 from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped
 
@@ -7,24 +8,27 @@ from geometry_msgs.msg import TwistStamped
 class DS4Subscriber(Node):
     def __init__(self):
         super().__init__('DS4Subscriber')
+        self._serial = serial.Serial('/dev/ttyACM0', baudrate=9600)
         self._subscription = self.create_subscription(
             TwistStamped, 'cmd_vel', self.listener_callback, qos_profile=10)
         self.get_logger().info('DS4 Bridge created successfully')
 
     def listener_callback(self, msg: TwistStamped):
-        self.get_logger().info('Got: %s' % msg.twist.linear)
+        self.get_logger().info('Caught message')
+        controlObject = {'x': msg.twist.linear.x, 'y': msg.twist.linear.z}
+        self._serial.write(json.dumps(controlObject).encode('ascii'))
+
+    def close_serial(self):
+        self._serial.close()
 
 
 def main():
-    ser = serial.Serial('/dev/ttyUSB0')
-    print(ser.name)
-    ser.write(b"HELLO!")
     rclpy.init(args=None)
     sub = DS4Subscriber()
     rclpy.spin(sub)
+    sub.close_serial()
     sub.destroy_node()
     rclpy.shutdown()
-    ser.close()
 
 
 if __name__ == '__main__':
