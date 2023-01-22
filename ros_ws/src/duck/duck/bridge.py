4d55ba2a77e949f62b6c8f83663205dfa16b3721
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-01-22 09:54:41 -0300

    Updates to serial port stuff

--- ros_ws/src/duck/duck/bridge.py
+++ ros_ws/src/duck/duck/bridge.py
@@ -3,23 +3,34 @@ import serial
 import json
 from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped
-
+from datetime import datetime
 
 class DS4Subscriber(Node):
     def __init__(self):
         super().__init__('DS4Subscriber')
-        self._serial = serial.Serial('/dev/ttyACM0', baudrate=9600)
+        self._serial = serial.Serial('/dev/ttyACM0', baudrate=9600, write_timeout=1)
         self._subscription = self.create_subscription(
             TwistStamped, 'cmd_vel', self.listener_callback, qos_profile=10)
         self.get_logger().info('DS4 Bridge created successfully')
+        self._lastTime = datetime.now()
 
     def listener_callback(self, msg: TwistStamped):
-        controlObject = {'x': msg.twist.linear.y, 'y': msg.twist.linear.z}
+        controlObject = {'x': msg.twist.linear.y*2, 'y': msg.twist.linear.z/2}
+        now = datetime.now()
+        delta = now - self._lastTime
+        if delta.microseconds < 100000:
+            return
+        self._lastTime = now
+
         self.get_logger().info(json.dumps(controlObject))
-        if (self._serial.isOpen()):
-            self._serial.write(json.dumps(controlObject).encode('ascii'))
-        else:
-            self.get_logger().info('[ERROR] Serial port is not open')
+        try:
+            if (self._serial.isOpen()):
+                self._serial.write(json.dumps(controlObject).encode('ascii'))
+            else:
+                self.get_logger().info('[ERROR] Serial port is not open')
+        except:
+            self.get_logger().error('Timed out')
+        # self._serial.close()
 
     def close_serial(self):
         self._serial.close()
